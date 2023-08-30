889a108279ebfbb0260ca9e23b9982aef6d0bafe
Oleksandr Nykytin <keeper@ninydev.com>
2023-08-30 20:13:59 +0300

    Connect to DB

--- hello_java/src/main/java/org/itstep/myClassWork/august30/August30MySql.java
+++ hello_java/src/main/java/org/itstep/myClassWork/august30/August30MySql.java
@@ -2,10 +2,14 @@ package org.itstep.myClassWork.august30;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+import java.util.Properties;
 
 
 public class August30MySql implements Runnable
@@ -14,7 +18,28 @@ public class August30MySql implements Runnable
 
     @Override
     public void run() {
-        ConnectToDb();
+        ConnectToDbByProps();
+    }
+
+    private void ConnectToDbByProps() {
+        Properties props = new Properties();
+        try(InputStream in = Files.newInputStream(Paths.get("database.properties"))){
+            props.load(in);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String url = props.getProperty("url");
+        String username = props.getProperty("username");
+        String password = props.getProperty("password");
+
+        try {
+            this.connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected to the database!");
+            // connection.close(); // Не забудьте закрыть соединение, когда оно больше не нужно
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     private void ConnectToDb() {
