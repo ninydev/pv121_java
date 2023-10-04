456762e8ccb1d38d692cb9b7f0de8f88fb77d50f
Oleksandr Nykytin <keeper@ninydev.com>
2023-10-04 20:53:58 +0300

    Init Project

--- GraphQL/first_server/index.js
+++ GraphQL/first_server/index.js
@@ -1,18 +1,19 @@
-
-
 // Создадим веб сервер
 const express = require('express');
 const app = express();
 
 
-
 // Создадим схему (правила ответов на запросы фронта)
-const {GraphQLSchema, GraphQLObjectType} = require("graphql");
+const {GraphQLSchema, GraphQLObjectType, GraphQLString} = require("graphql");
 const graphqlSchema = new GraphQLSchema({
 
     query: new GraphQLObjectType({
-        name: 'Root',
+        name: 'RootQueryType',
         fields: {
+            sayHello: {
+                type: GraphQLString,
+                resolve: () => 'Hello'
+            }
 
         }
 
@@ -21,6 +22,8 @@ const graphqlSchema = new GraphQLSchema({
 
 // Подключим правила ответов к нашему веб серверу
 const {graphqlHTTP} = require("express-graphql");
+// Вместо того, что бы писать разные маршруты для работы с разными сущностями
+// мы описываем 1 точку входа - все запросы теперь идут сюда
 app.use('/graphql', graphqlHTTP({
     graphqlSchema,
     graphiql: false
@@ -28,11 +31,12 @@ app.use('/graphql', graphqlHTTP({
 
 
 // Создадим раздел для документации (по доступным решениям)
+// Что бы фронты знали, на какие запросы я могу отвечать - стаим утилиту для документации
 const playGround = require('graphql-playground-middleware-express').default;
 app.use('/playground', playGround({endpoint:'/graphql'}));
 
 
 // Запустим веб сервер
 app.listen(3000, () => {
-    console.log("http://localhost:3000 Started");
+    console.log("http://localhost:3000/playground Started");
 })
