6eb3032c286df6ea2c56403d18e23a1dea2b5b5b
Oleksandr Nykytin <keeper@ninydev.com>
2023-10-06 20:53:51 +0300

    Xampp

--- dockers/tmp/app/public/index.php
+++ dockers/tmp/app/public/index.php
@@ -1,3 +1,6 @@
 <?php
+// Подключаю автозагрузку классов
+require_once "../vendor/autoload.php";
+
 echo "<h1>Hello World</h1> - <p>Я монолитное приложение под PHP</p>";
 require_once "../lib/info.php";
