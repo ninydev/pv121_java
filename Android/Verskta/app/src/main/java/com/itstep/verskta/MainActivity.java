e429b625415fbd7e15064129d39418a607c69cc9
Oleksandr Nykytin <keeper@ninydev.com>
2023-10-20 20:05:24 +0300

    Layouts

--- Android/Verskta/app/src/main/java/com/itstep/verskta/MainActivity.java
+++ Android/Verskta/app/src/main/java/com/itstep/verskta/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_linear_layout);
+        setContentView(R.layout.main_frame_layout);
     }
 }
