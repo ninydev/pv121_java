f0ffc6131c513e33c94a50eadeffb6224431260f
Oleksandr Nykytin <keeper@ninydev.com>
2023-10-21 18:39:57 +0300

    Жизненный цикл Activity

--- Android/Verskta/app/src/main/java/com/itstep/verskta/MainActivity.java
+++ Android/Verskta/app/src/main/java/com/itstep/verskta/MainActivity.java
@@ -3,13 +3,65 @@ package com.itstep.verskta;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected void log(String msg) {
+        Log.d("MainActivity", msg);
+    }
+
+    protected void toast(String msg) {
+        Toast.makeText(this.getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+    }
+
+
+    public MainActivity(){
+        super();
+        log("Constructor");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("TagName", "MsgName");
+        log("onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        log("onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        log("onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this.getApplicationContext(), "THK U Return", Toast.LENGTH_LONG).show();
+        log("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        log("onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        log("onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        log("onDestroy");
     }
 }
